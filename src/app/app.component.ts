b0926768cda0e9875dc66aee1d467d9b5872a1ca
aljuarizmi <aljuarizmi@gmail.com>
2025-01-24 13:26:51 -0500

    Cambios en app angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import { EmpleadosComponent } from "./empleados/empleados.component";
 
 @Component({
   selector: 'app-raiz',
@@ -9,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'appbund';
+  saludos='Hola a todo el mundo';
 }
---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+//import { EmpleadosComponent } from "../empleados/empleados.component";
+
+@Component({
+    selector:"app-empleados",
+    templateUrl:"./empleados.component.html",
+    styleUrl:"./empleados.component.css"
+    //imports: [EmpleadosComponent]
+})
+export class EmpleadosComponent{
+
+}
