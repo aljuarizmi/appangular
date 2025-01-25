91fb6a491f9e36c68d85efbdd7585e9d9be14360
aljuarizmi <aljuarizmi@gmail.com>
2025-01-25 15:14:53 -0500

    Mas cambios a appbund

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-empleado',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './empleado.component.html',
   //styleUrl: './empleado.component.css'
   styles:["p{background-color:red}"]
@@ -10,11 +11,24 @@ import { Component } from '@angular/core';
 export class EmpleadoComponent {
 private nombre="Jackie Giovanni";
 private apellido="Mora Nonato";
-private distrito="San Juan de Lurigancho";
+distrito="San Juan de Miraflores";
 habilitacionCuadro=true;
 usuRegistrado=false;
+textoDeRegistro="No hay nadie registrado";
 getRegistroUsuario(){
-  this.usuRegistrado=true;
+  this.usuRegistrado=false;
+}
+cambiaDistrito(event:Event){
+  this.distrito=(<HTMLInputElement>event.target).value
+}
+setUsuarioRegistrado(event:Event){
+  //alert("El usuario se acaba de registrar");
+  //this.textoDeRegistro="EL usuario se acaba de registrar";
+  if((<HTMLInputElement>event.target).value=="si"){
+    this.textoDeRegistro="El usuario se acaba de registrar";
+  }else{
+    this.textoDeRegistro="No hay nadie registrado";
+  }
 }
 getNombre(){
   return this.nombre
